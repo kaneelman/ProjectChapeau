a6fa8ea3b5c9f0b0777aa891085148e14c48cacb
stephenpangga <42969061+stephenpangga@users.noreply.github.com>
2019-05-22 14:21:45 +0200

    trial 3

--- ChapeauUI/Form1.Designer.cs
+++ ChapeauUI/Form1.Designer.cs
@@ -29,26 +29,35 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(123, 166);
-            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Location = new System.Drawing.Point(109, 133);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(151, 149);
+            this.button1.Size = new System.Drawing.Size(134, 119);
             this.button1.TabIndex = 0;
             this.button1.Text = "trial trial";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(488, 133);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(132, 119);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Trial 3 333";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(900, 562);
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -58,6 +67,7 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
