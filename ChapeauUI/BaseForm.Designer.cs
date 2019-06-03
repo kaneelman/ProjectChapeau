d9c6401c34445b4f5ee0c0abbf093beda378c4a8
Japheth Fenton <633240@student.inholland.nl>
2019-06-03 13:06:54 +0200

    trying some stuff with window size

--- ChapeauUI/BaseForm.Designer.cs
+++ ChapeauUI/BaseForm.Designer.cs
@@ -37,32 +37,30 @@
             // picBox_Chapeau
             // 
             this.picBox_Chapeau.Image = ((System.Drawing.Image)(resources.GetObject("picBox_Chapeau.Image")));
-            this.picBox_Chapeau.Location = new System.Drawing.Point(131, 78);
-            this.picBox_Chapeau.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.picBox_Chapeau.Location = new System.Drawing.Point(196, 120);
+            this.picBox_Chapeau.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
             this.picBox_Chapeau.Name = "picBox_Chapeau";
-            this.picBox_Chapeau.Size = new System.Drawing.Size(398, 224);
+            this.picBox_Chapeau.Size = new System.Drawing.Size(597, 345);
             this.picBox_Chapeau.TabIndex = 6;
             this.picBox_Chapeau.TabStop = false;
             // 
             // Btn_LogOut
             // 
             this.Btn_LogOut.CausesValidation = false;
-            this.Btn_LogOut.Location = new System.Drawing.Point(586, 11);
-            this.Btn_LogOut.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Btn_LogOut.Location = new System.Drawing.Point(879, 17);
             this.Btn_LogOut.Name = "Btn_LogOut";
-            this.Btn_LogOut.Size = new System.Drawing.Size(71, 40);
+            this.Btn_LogOut.Size = new System.Drawing.Size(106, 62);
             this.Btn_LogOut.TabIndex = 7;
             this.Btn_LogOut.Text = "LogOut";
             this.Btn_LogOut.UseVisualStyleBackColor = true;
             // 
             // BaseForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(668, 456);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(1002, 712);
             this.Controls.Add(this.Btn_LogOut);
             this.Controls.Add(this.picBox_Chapeau);
-            this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
             this.Name = "BaseForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "BaseForm";
