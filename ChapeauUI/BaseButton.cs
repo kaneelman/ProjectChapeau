e9d7053fc47a9d14dc9b5394fe64ba21e3035158
Japheth Fenton <633240@student.inholland.nl>
2019-06-03 16:21:38 +0200

    Basebutton works

--- ChapeauUI/BaseButton.cs
+++ ChapeauUI/BaseButton.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,9 @@ namespace ChapeauUI
 {
     public class BaseButton : Button
     {
-
+        public BaseButton()
+        {
+            Font = new Font("Arial", 14, FontStyle.Bold);
+        }
     }
 }
