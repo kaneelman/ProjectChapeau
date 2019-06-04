eaa1776f1a0f8093a56b90488a006e8ee254d3cf
stephenpangga <42969061+stephenpangga@users.noreply.github.com>
2019-06-04 16:40:06 +0200

    till fixing the listview

--- ChapeauUI/PaymentForm.cs
+++ ChapeauUI/PaymentForm.cs
@@ -45,9 +45,7 @@ namespace ChapeauUI
                 li.SubItems.Add(m.Quantity.ToString());
                 li.SubItems.Add(m.GetMenuItem().Price.ToString("0.00"));
                 lst_Payment.Items.Add(li);
-            }
-
-            
+            }            
         }
 
         private void btn_Pay_Click(object sender, EventArgs e)
