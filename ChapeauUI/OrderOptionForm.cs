32c1ba4d76da62a3cf769cf1e07197e084b7f1bf
stephenpangga <42969061+stephenpangga@users.noreply.github.com>
2019-06-13 13:33:19 +0200

    some fix

--- ChapeauUI/OrderOptionForm.cs
+++ ChapeauUI/OrderOptionForm.cs
@@ -92,12 +92,13 @@ namespace ChapeauUI
 
                 lst_CurrentOrder.Clear();
                 ListViewDesignOrderOption();
-                FillListView();//to update the listview when quantity change.. 
+                FillListView();//to update the listview when quantity change..                 
             }
             catch (Exception msg)
             {
                 MessageBox.Show(msg.Message);
             }
+
 
         }
 
@@ -123,7 +124,7 @@ namespace ChapeauUI
 
         private void Btn_NewOrder_Click(object sender, EventArgs e)
         {            
-            OrderForm orderForm = new OrderForm(LoggedInEmployee, this.loginForm, tableView);
+            OrderForm orderForm = new OrderForm(LoggedInEmployee, this.loginForm, tableView, order.Table);
             orderForm.ShowDialog();
             Close();
         }
